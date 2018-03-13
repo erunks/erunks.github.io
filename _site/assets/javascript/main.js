73f9ce8070be8d3e54664702c6bd3bbbc71e1649
Edward Runkel <Edward.Runkel@macys.com>
2018-03-13 17:09:11 -0400

    Visual changes to the nav menu

--- _site/assets/javascript/main.js
+++ _site/assets/javascript/main.js
@@ -1,9 +1,14 @@
 function toggleNav(){
   var nav = $('.nav-menu')[0];
+  var icon = $('.nav-menu-icon')[0];
   if(nav.classList.contains("hidden")){
     nav.classList.remove("hidden");
+    icon.classList.remove("fa-bars");
+    icon.classList.add("fa-times");
   }
   else{
     nav.classList.add("hidden");
+    icon.classList.add("fa-bars");
+    icon.classList.remove("fa-times");
   }
 };
