425100caa1167f72ee445a7b4723dd729c3be95f
Edward Runkel <Edward.Runkel@macys.com>
2018-03-14 10:03:02 -0400

    Added simple animations to the nav-menu

--- _site/assets/javascript/main.js
+++ _site/assets/javascript/main.js
@@ -1,14 +1,36 @@
 function toggleNav(){
   var nav = $('.nav-menu')[0];
   var icon = $('.nav-menu-icon')[0];
+  var screenOverlay = $('.overlay-screen')[0];
+
+  $(nav).on('animationend',function(event){
+    if(nav.classList.contains("slide-out")){
+      nav.classList.remove("slide-out");
+      nav.classList.add("hidden");
+      screenOverlay.classList.add("hidden");
+    }
+    else if(nav.classList.contains("slide-in")){
+      nav.classList.remove("slide-in");
+    }
+  });
+
+  $(screenOverlay).on('animationend',function(event){
+    if(screenOverlay.classList.contains("fade-out")){
+      screenOverlay.classList.remove("fade-out");
+    }
+    else if(screenOverlay.classList.contains("fade-in")){
+      screenOverlay.classList.remove("fade-in");
+    }
+  });
+
   if(nav.classList.contains("hidden")){
     nav.classList.remove("hidden");
-    icon.classList.remove("fa-bars");
-    icon.classList.add("fa-times");
+    screenOverlay.classList.remove("hidden");
+    nav.classList.add("slide-in");
+    screenOverlay.classList.add("fade-in");
   }
   else{
-    nav.classList.add("hidden");
-    icon.classList.add("fa-bars");
-    icon.classList.remove("fa-times");
+    nav.classList.add("slide-out");
+    screenOverlay.classList.add("fade-out");
   }
 };
