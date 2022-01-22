5a36c36c2ae8eefe3244e7e325ca0eb0f6bc1076
erunks <eddierunkel@gmail.com>
2022-01-22 16:52:15 +0000

    Deploying to gh-pages from @ erunks/erunks.github.io@f9061848382a064c64e17aece087fe92c17376d3 🚀

--- _next/static/_-QRp_Gwfc1cWewX8WU7S/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/80-484bf7b35df33836.js","static/css/ffd835c4cc30f28d.css","static/chunks/pages/index-5fbd0ca321388564.js"],"/_error":["static/chunks/pages/_error-9734db4c9fd59614.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/sMtNIqBQ3tGqZvCQqk2Lm/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/80-910cb4e6cee545e6.js","static/css/ffd835c4cc30f28d.css","static/chunks/pages/index-5fbd0ca321388564.js"],"/_error":["static/chunks/pages/_error-9734db4c9fd59614.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
