fa270a1a683ee2ce019b24ddd16df4829e5fc4c8
erunks <eddierunkel@gmail.com>
2023-02-18 02:28:55 +0000

    Deploying to gh-pages from @ erunks/erunks.github.io@bf7573555d189a141624f471ad6025b6f113ed88 🚀

---
+++ _next/static/xmQiNDpyUiv4vn-JUpINn/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/293-5d7c67e01042204a.js","static/css/ffd835c4cc30f28d.css","static/chunks/pages/index-0bfb0981db932201.js"],"/_error":["static/chunks/pages/_error-9734db4c9fd59614.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
